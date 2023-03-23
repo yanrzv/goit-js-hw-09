3509765b18675f647e0d51bed23bd7efa64ca0c7
Yan Razvadovskyi <116461898+yanrzv@users.noreply.github.com>
2023-03-23 04:12:15 +0200

    first done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let interval = 0;
 
+
+startBtn.addEventListener('click', onStartButton);
+stopBtn.addEventListener('click', onStopButton);
+
+
+function onStartButton() {
+    startBtn.disabled = true;
+
+    interval = setInterval(() =>
+        body.style.backgroundColor = getRandomHexColor(), 1000);
+}
+
+function onStopButton() {
+    startBtn.disabled = false;
+    clearInterval(interval);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
